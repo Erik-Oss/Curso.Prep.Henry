d5c05657e9b42afe91d9dab1ef7d092ffb19655b
Erik-18 <85121942+Erik-18@users.noreply.github.com>
2021-09-24 12:21:38 -0300

    Commit n°2

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,25 +45,24 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var array = [];
-  cb(array.forEach());
-  newarray = [];
-  newarray.push(cb);
+var newArray = array.map(function(a) {
+  return cb(a);
+});
+return newArray;
 }
 
-  // var array = [1,2,3,4,5]
-  // function cb (e) {console.log(e)}
-  // nuevoarray = []
-  // var nuevoArray = [];
-  // for(var i = 0; i < array.length; i++) {
-  //   nuevoArray.push(cb(array[i]));
-  //   nuevoArray[i] = cb(array[i]);
-  // }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var nuevoArray = [];
+  for(let i = 0; i<array.length; i++) {
+    if(array[i][0] === "a") {
+      nuevoArray.push(array[i])
+    }
+  }
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
